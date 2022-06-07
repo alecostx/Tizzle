87cecf9895b345b96339e727945398cd9e9896f4
alecostx <alexandre.rodrigues@sptech.school>
2022-06-07 19:42:11 -0300

    Commit final local

---
+++ insert-frank.sql
@@ -0,0 +1,120 @@
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Porter Donaldson","ullamcorper.nisl.arcu@outlook.edu","2023-01-07","86383582484","LDW44SGE6HU",4),
+  ("Illiana Ayers","mus.proin@aol.edu","2023-05-18","30382268098","SAS86NQQ2PF",4),
+  ("Ahmed Hooper","nec.cursus.a@aol.org","2021-09-13","35234786557","SMJ08TNW2OO",4),
+  ("Henry Clarke","cum.sociis@icloud.org","2021-10-15","88927270218","SJH05IXJ1AM",4),
+  ("Quynn Klein","iaculis.quis@icloud.edu","2021-08-20","67706152725","BEQ57JGX9GN",4),
+  ("Ifeoma Lawrence","mauris.sit@outlook.edu","2022-02-10","53852996483","CCC78KPU6CK",4),
+  ("Yoshio Mercer","condimentum@icloud.net","2021-11-22","77172962917","XHP36UOT8VC",4),
+  ("Derek Rowland","rhoncus.nullam@aol.ca","2022-07-08","23927709071","TWE32ZTX3HO",4),
+  ("Scott Burt","magna.a.tortor@hotmail.edu","2021-07-13","85501063210","OEB14OPT2XO",4),
+  ("Kareem Cummings","fringilla.ornare@protonmail.ca","2021-12-30","74134261375","CPK99ENZ7XW",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Kermit Craft","consectetuer.adipiscing@aol.edu","2023-04-21","10287837643","MXK65QOS9KH",4),
+  ("Christian Goodman","sed.pede@icloud.couk","2022-01-03","26088327117","QLG36NTU2LH",4),
+  ("Kameko Noel","donec.sollicitudin.adipiscing@google.edu","2022-03-24","13344157206","WSQ53PFF2LE",4),
+  ("Jael Jenkins","augue.ut@protonmail.org","2022-07-09","82498337586","IIK05DSY2UY",4),
+  ("Hedley Stokes","curabitur@google.org","2021-06-15","23955592165","RLG25KPM2KS",4),
+  ("Logan Little","risus.varius@aol.edu","2022-12-15","91257627851","TTB62MFB6FI",4),
+  ("Uta Leon","vitae@protonmail.org","2021-08-10","97863068186","LXE26XRF6ZV",4),
+  ("Priscilla Nicholson","neque.sed.dictum@outlook.edu","2022-07-11","21434110365","LLB26LGQ4DY",4),
+  ("Dale Huber","tempor.erat.neque@yahoo.couk","2021-09-16","83871832938","GUI14PWL4WD",4),
+  ("Inez Le","nullam.vitae@aol.com","2021-12-17","85649813126","ODV21QDC4DG",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Willow Cherry","mus@google.ca","2022-06-01","40226755332","FYT64BJX7LN",4),
+  ("Addison Tyson","enim@google.ca","2023-03-29","64933110852","KPL22CWF2WM",4),
+  ("Daria Howe","sed.auctor.odio@icloud.ca","2022-04-10","18281255938","LTV42FLY9FV",4),
+  ("Zena Strong","interdum.curabitur@icloud.couk","2022-04-02","22503873108","HCM72OUL0QB",4),
+  ("Nolan Fernandez","ornare@protonmail.edu","2021-09-10","32541237997","MHN11YQE6EO",4),
+  ("Tanisha Clemons","fringilla.cursus.purus@icloud.ca","2023-04-12","12707929017","VQT85YXN0KD",4),
+  ("Donna Wade","vivamus.nisi@yahoo.net","2022-07-26","32554414996","PSK13OTX0YE",4),
+  ("Veronica O'connor","lacus.quisque@hotmail.net","2021-12-26","75886386015","CWM21FLB1XU",4),
+  ("Timothy Duke","arcu.sed@hotmail.com","2023-06-06","43163127806","LGK28DQQ8JX",4),
+  ("Maxine Hensley","diam.proin@protonmail.ca","2022-07-25","91012161727","GHI54SBQ2DF",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Buckminster Bridges","curabitur@yahoo.couk","2022-06-24","79340389402","GAG16FIF5DW",4),
+  ("Owen Mcneil","integer.sem@icloud.net","2021-07-25","17343273336","ICN87DDP5OI",4),
+  ("Rajah Chavez","quisque@yahoo.ca","2021-07-08","23466122491","BML08JUH1DV",4),
+  ("Duncan Woodward","vitae.sodales.at@protonmail.couk","2022-11-03","70424487153","GTN96HGR9LZ",4),
+  ("Ian Johns","posuere.at@aol.org","2021-09-03","91348784913","EHZ87GQA2NS",4),
+  ("Ulysses Alexander","nam.porttitor.scelerisque@aol.couk","2023-02-21","18001416261","XUR16GGV4XH",4),
+  ("Brock Shannon","et@protonmail.net","2022-11-07","50592828122","UEX00PCD7OL",4),
+  ("Colt Dyer","ut.pellentesque@aol.couk","2022-05-30","06558745370","TOC94RKU1DP",4),
+  ("Hop Taylor","donec.tincidunt.donec@hotmail.org","2022-03-12","63528302404","JTU14EXA5KI",4),
+  ("Lester Navarro","tellus.sem.mollis@aol.edu","2023-05-28","75995719427","FGS61TFE6GC",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Yuli Beach","sem.ut@hotmail.net","2022-05-21","15369941286","NAD35BRT3DI",4),
+  ("Miranda Nieves","lobortis@google.com","2021-10-20","93722233027","KMQ35QUC5IN",4),
+  ("Wayne Phelps","vel@yahoo.edu","2023-03-16","62679991411","TMG31XKH6US",4),
+  ("Farrah Donaldson","nulla@aol.net","2021-06-28","91415112618","XMT16IRR8IP",4),
+  ("Cody Cotton","phasellus.dolor.elit@icloud.couk","2022-02-19","26421893448","ESE86XJY6OM",4),
+  ("Holmes Lawrence","mauris@yahoo.couk","2022-09-20","12154541744","LIF22CVJ7KR",4),
+  ("Darryl Dixon","senectus.et.netus@aol.net","2022-08-01","79853444962","NMK76KKX5NL",4),
+  ("Louis Spencer","dolor.quisque.tincidunt@google.org","2021-09-26","25551251787","TIR65RTY1WO",4),
+  ("Quail Francis","tellus.justo@outlook.org","2023-02-08","12721916293","IKG58BGJ3ST",4),
+  ("Amy Mathews","ullamcorper.velit.in@google.com","2023-05-24","57187980042","NKQ43BHB7FU",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Micah White","lacus.quisque@icloud.ca","2023-05-06","28294286642","TMR74PWE6US",4),
+  ("Thane Fowler","velit.cras@outlook.couk","2023-02-11","77253417887","ZTI76NKK8YW",4),
+  ("Russell Anderson","leo.elementum.sem@google.ca","2021-09-15","66430654221","WQI68OSY4ID",4),
+  ("Guy Scott","luctus.et@yahoo.org","2022-07-10","25118551137","PED77EKH2BO",4),
+  ("Jelani Hicks","condimentum.eget@yahoo.ca","2023-02-21","78822554511","PTH52NXU4JG",4),
+  ("Garth Phelps","consectetuer.adipiscing@icloud.ca","2022-01-10","90732244170","BPF46MOW5GL",4),
+  ("Wylie Cantu","tempus.eu@hotmail.org","2023-04-19","71886272477","UDA03EBS2BJ",4),
+  ("Ina Horn","aliquet.lobortis@outlook.net","2023-05-27","41053942447","IDQ43CYK6CX",4),
+  ("Asher Romero","mollis@aol.edu","2022-04-17","13161303183","DNZ18RBQ4ID",4),
+  ("Aladdin Sullivan","nulla.tincidunt@yahoo.ca","2021-10-21","56504729135","GNR53NVC7HU",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Edan Clemons","nunc.est@hotmail.edu","2022-04-24","85489572429","KBB48ILR6XH",4),
+  ("Aquila Walker","purus.ac@outlook.ca","2022-02-23","88778917353","NEV65PNY6CW",4),
+  ("Ivan Hardin","integer.id@icloud.couk","2023-02-15","81863542645","PRC34FWU4DU",4),
+  ("Abraham Byrd","tellus.phasellus@yahoo.com","2022-10-23","27084287736","GWB72ZTS8DN",4),
+  ("Griffin Cote","in.lobortis@hotmail.couk","2022-01-27","83164391785","LSF44UHB4UU",4),
+  ("Lareina Yang","commodo@yahoo.com","2022-05-16","47671365554","ZRE34RWA2SW",4),
+  ("Leilani Barlow","mi.duis@yahoo.couk","2021-07-01","57964771742","UPW55NAC6CQ",4),
+  ("Adele Booth","lacus@protonmail.ca","2021-09-01","81233602029","YAL62PPM2VB",4),
+  ("Sonya Britt","nisi.magna.sed@outlook.ca","2022-04-14","64293851243","DGI48EKP5WI",4),
+  ("Lamar Jackson","gravida.sagittis.duis@icloud.com","2023-04-06","87435275708","CEP24OZW5UF",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Harlan Morales","accumsan.convallis.ante@icloud.couk","2023-02-03","38293988467","JKG75BXJ1CV",4),
+  ("Alana Hopper","magna.lorem@protonmail.edu","2022-05-02","52613196928","GMJ28MVB3YS",4),
+  ("Hilda Conrad","mi.eleifend.egestas@google.net","2022-09-24","28113288812","XBH42QHM3EZ",4),
+  ("Chava Mendoza","non.feugiat.nec@yahoo.com","2021-07-08","33262172930","CGP37HZB9GN",4),
+  ("Patrick Alford","quisque.tincidunt@aol.ca","2021-11-17","36534492539","STN56MQI6LZ",4),
+  ("Morgan Ayala","aliquet.metus@icloud.net","2022-05-09","82448841928","MIJ28RWT5CG",4),
+  ("Dante Jimenez","nisl.elementum.purus@outlook.couk","2023-02-05","22747121819","SUR70SKP6VN",4),
+  ("Urielle Mendoza","blandit@yahoo.couk","2023-02-21","54211055809","RXS34EMS5TG",4),
+  ("Minerva Serrano","ultricies.adipiscing@aol.net","2022-04-27","77642657733","ULD03IJM4JT",4),
+  ("Harlan Valentine","nec.ligula.consectetuer@google.com","2023-06-06","20176545884","MKZ52EGZ6JI",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Peter Townsend","quis@protonmail.couk","2023-05-26","04596012347","EWP73CEW5WB",4),
+  ("Ann Neal","enim.sit.amet@aol.edu","2022-04-14","74877182519","SRW26JRL4GT",4),
+  ("Rhona Kerr","hymenaeos@aol.com","2021-10-15","03054977258","HWX98WJV0FG",4),
+  ("Malachi Holmes","iaculis.lacus@google.edu","2022-12-10","98636723446","VYM88SUO9HE",4),
+  ("Madonna Richardson","consequat@yahoo.couk","2021-07-19","48357303452","ZSV32INF6FQ",4),
+  ("August Gomez","mi.lacinia@google.couk","2022-07-09","81256105656","FEO05ACJ8YD",4),
+  ("Clare Horn","fusce.mollis@google.ca","2023-01-28","56159326285","CQN43XZU8FC",4),
+  ("Tatum Coleman","cursus.luctus@aol.edu","2022-08-16","30832715860","KZN61MIE1VT",4),
+  ("Harriet Jimenez","velit.pellentesque@hotmail.edu","2021-11-11","46962508588","IWC16KYW8KT",4),
+  ("Risa Skinner","eleifend.egestas.sed@hotmail.org","2022-05-29","44116643979","VBK49OFT8LR",4);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Wayne Lynn","dictum.sapien@protonmail.edu","2022-02-08","44266864545","OBY38XNY4BO",4),
+  ("Imelda Poole","faucibus.id@yahoo.net","2021-11-02","74742657538","YQU25LLC1JE",4),
+  ("Callie Good","phasellus@google.edu","2022-08-28","87167459687","OGV32XGH7NC",4),
+  ("Hop Harrell","at@google.couk","2022-08-25","47504870614","RFG66KVE0EG",4),
+  ("Thomas Bradshaw","etiam.ligula@outlook.org","2022-11-06","63808642467","HTH43OPR7LY",4),
+  ("Elizabeth Coffey","vestibulum.massa@outlook.net","2021-12-30","72666123228","JEV01XLT3VS",4),
+  ("Evelyn William","eleifend.non@yahoo.edu","2021-08-22","20253178003","PMT03WRL5BZ",4),
+  ("Audrey Solomon","netus.et.malesuada@aol.com","2022-05-31","74619946722","KVI78HPT4CV",4),
+  ("Ralph Chen","lorem.ipsum.dolor@outlook.net","2021-06-12","84218233227","DJV75WFW0AE",4),
+  ("Hayes Oneil","in.faucibus@protonmail.edu","2022-04-05","43787513663","LEO35UDE1JI",4);
---
+++ insert-melly.sql
@@ -0,0 +1,91 @@
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Phyllis Donovan","integer.sem@hotmail.edu","2022-04-30","11677019774","FYN22WSK8QM",5),
+  ("Xanthus Daugherty","ipsum.nunc@aol.couk","2022-02-11","76180405381","UWO82FBJ7DR",5),
+  ("Joseph Barnes","vitae@google.com","2022-09-21","71914411258","MVO53RDX4ZH",5),
+  ("Lynn Cotton","erat@yahoo.com","2023-01-14","61946726641","MRN37KNZ8XM",5),
+  ("Molly Dennis","libero.dui.nec@hotmail.edu","2022-11-07","86658548885","BWL67WUP3JC",5),
+  ("Brennan Mendez","non.lobortis.quis@icloud.org","2021-11-03","27713211529","VIH79ZMK2UA",5),
+  ("Noble Stephens","sed.pede@google.org","2022-10-01","42271215368","LAC04OLN8EK",5),
+  ("Beau Morrison","tempus.scelerisque@yahoo.ca","2021-08-16","41451212132","ZAJ20XYB1GC",5),
+  ("Myra Mckenzie","mus.aenean@aol.com","2022-11-10","77862868753","TEP99KCX2CN",5),
+  ("Andrew Cobb","suspendisse.sagittis@outlook.net","2023-03-02","23737669048","XGG26HPX8SD",5);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Berk Rowland","fermentum.fermentum.arcu@yahoo.ca","2022-01-07","00510621066","ZKS88TOD1MM",5),
+  ("Yetta Steele","ligula.nullam@icloud.net","2021-10-05","60670472865","TGW84DVB4IK",5),
+  ("Hedy Pace","non.enim.commodo@aol.com","2021-07-14","18741438613","LXK36GSL2ME",5),
+  ("Hanae Moon","magna@hotmail.net","2022-05-17","83428145618","GQW58GNK7FC",5),
+  ("Samson Blair","quisque.ac.libero@outlook.couk","2021-12-25","42375166248","THQ22KUG2HB",5),
+  ("Leandra Schroeder","nec@google.ca","2023-04-23","46773951577","BTW35OMN5BG",5),
+  ("Anne Walker","urna@yahoo.net","2022-07-10","59486051581","TUB71PUO6QM",5),
+  ("Mari Stark","nec.luctus@google.ca","2022-09-08","54421188151","VZM33PRO9MD",5),
+  ("Sade Stone","nam@yahoo.ca","2022-09-06","35588852175","XDQ08HVR7EF",5),
+  ("Clinton Moore","nam@aol.couk","2022-07-06","93764639821","KMT44PPR7OI",5);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Megan Mosley","ut.erat@aol.couk","2022-12-23","46357180855","AMS88QET8EG",5),
+  ("Jonah Shaffer","erat.etiam@yahoo.net","2022-09-06","71036375971","DVH24HWL7LF",5),
+  ("Alvin Compton","eu.odio@yahoo.com","2021-11-20","40882388597","ORX75LLD4XV",5),
+  ("Mohammad Ortega","elit@icloud.couk","2022-04-19","64632167752","DEV26OHB3KQ",5),
+  ("Norman Ayala","ac.eleifend.vitae@icloud.net","2022-10-07","76169995632","REX28OFY7DF",5),
+  ("Hoyt Finley","ante.vivamus.non@outlook.ca","2023-05-04","83223268518","IMH21CKY5KB",5),
+  ("Christen Sawyer","tincidunt.pede@outlook.ca","2022-12-12","18858772857","WML05EYC6UK",5),
+  ("Grace Carey","magnis.dis@yahoo.edu","2023-04-28","70521168743","WXA40KPT2CH",5),
+  ("May Wagner","mus@aol.ca","2022-07-02","81143141228","JIT96SBV7BK",5),
+  ("Craig Chen","interdum.curabitur.dictum@google.edu","2021-09-08","78554110048","LGT44VDQ1ND",5);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Elizabeth Davidson","donec.vitae@hotmail.edu","2022-07-13","26578037749","PCK44XTG3RO",5),
+  ("Ethan Sloan","nunc@icloud.couk","2023-01-23","64495799885","MLT83UCF9KV",5),
+  ("Dara Delaney","lorem.ipsum@outlook.net","2022-08-01","35471582500","KRQ91KTB2PM",5),
+  ("Buckminster Oneil","quisque.tincidunt.pede@protonmail.ca","2022-04-09","94534714772","HFJ56KCP9FQ",5),
+  ("Mufutau Frederick","ac.mattis@protonmail.couk","2022-06-04","97225690534","INQ88UVP0MD",5),
+  ("Florence Summers","lorem@hotmail.org","2021-12-17","04260863437","TAL05LRG5BT",5),
+  ("Giselle Ashley","nunc.mauris@icloud.ca","2022-09-25","58765802107","OHB92SXO8HX",5),
+  ("Mohammad Norton","varius.nam@icloud.ca","2022-02-19","24911651473","DGW85BXE8EK",5),
+  ("Nomlanga Byers","eleifend.non.dapibus@yahoo.ca","2022-08-31","56422848249","JNJ73DWG6RI",5),
+  ("Andrew Sheppard","vitae@protonmail.net","2022-10-25","22756956171","GEI73CAS3YP",5);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Cameron Calderon","elit.fermentum.risus@protonmail.edu","2021-08-27","61945338144","XJD17IUB1MN",5),
+  ("Drew Knowles","egestas@hotmail.edu","2021-12-10","42267815719","GTC82RGO5KT",5),
+  ("Tiger Fischer","aliquam.tincidunt@icloud.ca","2022-01-11","57353735148","KTX76FDQ9YX",5),
+  ("Kylie Craft","felis.ullamcorper.viverra@outlook.org","2022-09-17","52682671552","BBU01KMO2AX",5),
+  ("Jennifer Foreman","molestie@yahoo.edu","2021-11-08","25632248878","KBS48YLW4TK",5),
+  ("Charles Ferguson","in@yahoo.couk","2021-07-13","72828875432","VMK21HAE4RK",5),
+  ("Cecilia Henderson","interdum.ligula@hotmail.edu","2021-11-29","05285756941","XNL51WLV1AZ",5),
+  ("Dale Frederick","non.massa.non@google.edu","2023-02-24","54726828416","XHY14BMO1QQ",5),
+  ("Kiona Garza","nulla@yahoo.edu","2021-11-29","08889207235","MEP48PTP5CB",5),
+  ("Jakeem Blackburn","mauris.eu@protonmail.org","2022-10-10","77617849325","MPR59PSV1JN",5);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Mallory Le","bibendum.donec.felis@outlook.couk","2021-11-24","28844876885","VRR01TNR4NG",5),
+  ("Anjolie Trevino","purus.in.molestie@yahoo.org","2022-10-01","23863868743","OXN52RNO6DG",5),
+  ("Halla Barnett","lacus.varius@outlook.org","2022-01-22","35478747323","XMH47IVY3TN",5),
+  ("Ezekiel Mccray","justo@aol.edu","2022-03-22","29651984431","HDS17NMG8JO",5),
+  ("Gavin Walter","egestas.aliquam.nec@aol.ca","2021-09-13","86183078061","FUD11SKI5HW",5),
+  ("Aladdin Atkins","tellus.non@google.couk","2022-07-05","27495610747","UUX16HYQ8YV",5),
+  ("Cade Wagner","tempor.est.ac@icloud.edu","2023-03-08","76096558701","EKO70ONQ8QX",5),
+  ("Macy Rodriquez","neque@icloud.com","2022-10-28","66732136732","IEI47HLL3LU",5),
+  ("Driscoll Cruz","etiam.laoreet.libero@google.com","2023-04-30","34274724503","TYF33QCR2WD",5),
+  ("Drake Houston","ultricies.sem@hotmail.net","2022-10-25","71515567514","XES41QQJ4WF",5);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Erica Grimes","sed.congue@protonmail.org","2022-06-15","78624118552","WMJ76PPR3EB",5),
+  ("Judith Cobb","donec.luctus@hotmail.org","2021-11-19","82061440351","FWN80RTI7YB",5),
+  ("Jasmine Baker","orci@icloud.com","2021-10-09","96459035158","XCK79HMA6KR",5),
+  ("Harlan Watkins","non.hendrerit@outlook.edu","2022-04-01","83265537044","OEW84SFN4OY",5),
+  ("Jermaine Stark","magnis.dis@protonmail.ca","2021-08-02","64633661982","TOR23KBW7SQ",5),
+  ("Kellie Hood","donec@google.edu","2021-06-18","56420826128","HPN48HPG6FT",5),
+  ("Nicholas Hartman","nulla.donec.non@aol.ca","2022-04-10","89619811920","QQR68YRN3CQ",5),
+  ("McKenzie Casey","nam.ligula.elit@google.ca","2021-11-13","40610898252","KDX54HUK6OZ",5),
+  ("Justine Ratliff","amet.luctus@aol.net","2023-01-12","49382047348","PCV55FLB8XX",5),
+  ("Mohammad Pollard","cursus@yahoo.edu","2021-12-27","38881931169","RSO81OTD5VQ",5);
+INSERT INTO usuario (nome,email,dtNasc,cpf,senha,fkArtista)
+VALUES
+  ("Jamal Berry","dolor.sit.amet@hotmail.org","2023-01-14","45936522776","MMN22QTQ6ZB",5),
+  ("Phillip Patterson","nec@hotmail.com","2022-10-10","98326671826","HJJ58LRF3QA",5),
+  ("Daquan Saunders","dapibus.ligula.aliquam@hotmail.edu","2022-08-12","52170159412","KEO63QJV5FW",5),
+  ("Carly Fox","quam.vel@icloud.ca","2022-11-27","48667384757","WIS88FEI7KR",5),
+  ("Irma Bridges","per.inceptos.hymenaeos@hotmail.net","2022-01-21","71566332016","SDT66WRW5NJ",5);
